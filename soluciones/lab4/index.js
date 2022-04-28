2574d61165a99e474a4d847e624f8f2e66bcd316
Erick Vicente Agrazal Lopez <erick@agrazal.com>
2022-04-28 07:35:10 -0500

    Added a unique event listener to remove cards.

--- soluciones/lab4/index.js
+++ soluciones/lab4/index.js
@@ -7,23 +7,31 @@
         },
         init: () => {
             App.htmlElements.form.addEventListener('submit', App.handlers.onFormSubmit);
+            App.htmlElements.response.addEventListener('click', App.handlers.onCardClick);
         },
         utils: {
             ...Utils.methods,
         },
         templates: {
             card: (n) => {
-                return `<div class="card">${n}</div>`
+                return `<div class="card">${n}</div>`;
             }
         },
         handlers: {
+            onCardClick: (e) => {
+                if(e.target.className === 'card') {
+                    e.target.remove();
+                }
+            },
             onFormSubmit: (e) => {
                 e.preventDefault();
+
                 App.htmlElements.response.innerHTML = '';
+
                 const n = App.htmlElements.input.value;
-                App.utils.fibonacci(n).forEach(number => {
-                    App.htmlElements.response.innerHTML += App.templates.card(number);
-                })
+                App.utils.fibonacci(n).forEach(value => {
+                    App.htmlElements.response.innerHTML += App.templates.card(value);
+                });
             }
         }
     };
