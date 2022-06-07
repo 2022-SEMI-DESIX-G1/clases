074a87c19a176034d4739bf40575fc6d16d8e0cc
Erick Vicente Agrazal Lopez <erick@agrazal.com>
2022-06-06 20:07:43 -0500

    Added lab 8 example.

--- soluciones/lab8/app.js
+++ soluciones/lab8/app.js
@@ -1,21 +1,40 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+const axios = require("axios").default;
 
-app.get("/", function (req, res) {
-  res.json({ hello: "world" });
+const { PORT = 3000 } = process.env;
+
+const CACHE = {};
+const ERROR = {};
+
+app.get("/cache", function (req, res) {
+  res.json({ data: CACHE });
 });
 
-app.get("/hi", function (req, res) {
-  res.json({ hi: "hi" });
-});
-
-app.post("/hi", function (req, res) {
-  res.json({ hi: "hi" });
-});
-
-app.post("/pokemon/:name", function (req, res) {
+app.post("/pokemon/:name", async function (req, res) {
   const { name } = req.params;
-  res.json({ name });
+  if (CACHE[name]) {
+    return res.json({ name, data: JSON.parse(CACHE[name]), isCached: true });
+  }
+  if (ERROR[name]) {
+    return res.json({ name, data: JSON.parse(ERROR[name]), isCached: true });
+  }
+
+  const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
+  let responseData;
+  try {
+    const { data } = await axios.get(url);
+    responseData = data;
+    CACHE[name] = JSON.stringify(data);
+  } catch {
+    responseData = data;
+    ERROR[name] = JSON.stringify({ name, error: "Invalid pokemon." });
+  }
+  res.json({ name, data: responseData, isCached: false });
 });
 
-app.listen(3000);
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}...`);
+});
